23fe08c25a5cac6f440e0986ea1a39f6129b0985
王博伦 <657212135@qq.com>
2021-06-02 21:13:50 +0800

    test

---
+++ src/helloworld.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+
+}
